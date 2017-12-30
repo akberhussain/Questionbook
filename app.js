3bc9cf253395878b7dab4ede66ea5e4754fd5332
Akber Hussain <akber_leghari@hotmail.com>
2017-12-30 18:49:25 +0500

    added enviornment variable

--- app.js
+++ app.js
@@ -54,6 +54,6 @@ app.use(function(req, res, next){
 
 app.use(indexRoutes);
 
-	app.listen(3000, function(){
+	app.listen(process.env.PORT || 3000, function(){
     console.log("Questionbook app has started on port: 3000 !!");
 });
